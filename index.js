948c681e36f87620d94356b587c824fcd3cedd7f
Mario <marioalonsomora84@gmail.com>
2023-01-24 17:08:25 +0100

    Backend 3.5 completo

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use('/api/events',require('./routes/events'));
 
 app.get('*',(req, resp) =>{
 
-    resp.sendFile(__dirname + '/public/index.html');
+    resp.sendFile(__dirname + './public/index.html');
 
 })
 
