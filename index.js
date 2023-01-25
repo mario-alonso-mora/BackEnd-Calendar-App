a5bdcc0486cc0e12f326877a943b919d2dd35352
Mario <marioalonsomora84@gmail.com>
2023-01-25 12:03:34 +0100

    BackEnd 3.0 completed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/auth',require('./routes/auth'));
 
 app.use('/api/events',require('./routes/events'));
 
-app.post('*',(req, resp) =>{
+app.get('*',(req, resp) =>{
 
     resp.sendFile(__dirname + 'public/index.html');
 
