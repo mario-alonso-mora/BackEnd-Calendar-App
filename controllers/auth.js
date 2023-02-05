6626d77b79258dcfe5ac98ba338e6124bdadfe0b
Mario <marioalonsomora84@gmail.com>
2023-02-05 17:03:40 +0100

    new changes added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,159 +1,108 @@
-const {response} = require('express');
-const Users = require('../models/Users');
-const bcrypt = require('bcryptjs');
-const { generateJsonWebToken } = require('../helpers/jwt');
+const { response } = require("express");
+const Users = require("../models/Users");
+const bcrypt = require("bcryptjs");
+const { generateJsonWebToken } = require("../helpers/jwt");
 
+const createUser = async (request, resp = response) => {
+  const { email, password } = request.body;
 
+  try {
+    let user = await Users.findOne({ email });
 
-
-
-
-const createUser = async(request,resp = response ) =>{
-
-    const {email,password} = request.body;
-
-    try {
-
-        let user = await Users.findOne({email})
-
-        if(user){
-
-          return resp.status(400).json({
-                ok:false,
-                msg:'this email is already in use'
-            })
-
-        }
-        
-         user = new Users(request.body);
-
-         //Encriptar contraseña
-
-         const salt = bcrypt.genSaltSync();
-
-         user.password = bcrypt.hashSync(password,salt);
-
-        await user.save();
-        //Generar JWT (Json web token)
-       const token = await generateJsonWebToken(user.id,user.name);
-
-       return resp.status(201).json({
-
-            ok:true,
-            uid:user.id,
-            name:user.name,
-            token
-        
-        })
-
-    } catch (error) {
-
-        console.log(error)
-
-       return resp.status(500).json({
-
-            ok:false,
-            msg:'Please talk with the admin'
-
-        }) 
+    if (user) {
+      return resp.status(400).json({
+        ok: false,
+        msg: "this email is already in use",
+      });
     }
 
-};
+    user = new Users(request.body);
 
-const loginUser = async (request,resp = response ) =>{
+    //Encriptar contraseña
 
-    const {email,password} = request.body;
+    const salt = bcrypt.genSaltSync();
 
-    try {
+    user.password = bcrypt.hashSync(password, salt);
 
-       
-        const user = await Users.findOne({email})
+    await user.save();
+    //Generar JWT (Json web token)
+    const token = await generateJsonWebToken(user.id, user.name);
 
-        if(!user){
-
-           return resp.status(400).json({
-                ok:false,
-                msg:'this user dont exists with this email '
-            });
-
-        }
-
-        //Confirmar las contraseñas
-        const validPassword = bcrypt.compareSync(password,user.password);
-
-        if (!validPassword) {
-
-            return resp.status(400).json({
-
-                    ok:false,
-                    msg:'Password incorrect'
-
-            });
-            
-        }
-
-        //Generar nuestro JWT (Json web token)
-        const token = await generateJsonWebToken(user.id,user.name);
-
-       return resp.status(200).json({
-
-            ok:true,
-            uid:user.id,
-            name:user.name,
-            token
-
-            
-        });
-
-    
-
-
-        
-    } catch (error) {
-
-        console.log(error)
-
-       return resp.status(500).json({
-
-            ok:false,
-            msg:'Please talk with the admin'
-
-        })
-        
-    }
-  
-
-    
-};
-
-
-const revalidateToken = async(request,resp = response ) =>{
-
-    //   const uid = request.uid;
-    //   const name = request.name;
-
-    const {uid,name} = request;
-    const token = await generateJsonWebToken(uid,name);
-
-   return resp.status(200).json({
-
-        ok:true,
-        name,
-        uid,
-        token,
-        
-        
+    return resp.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      token,
     });
+  } catch (error) {
+    console.log(error);
 
+    return resp.status(500).json({
+      ok: false,
+      msg: "Please talk with the admin",
+    });
+  }
 };
 
+const loginUser = async (request, resp = response) => {
+  const { email, password } = request.body;
 
+  try {
+    const user = await Users.findOne({ email });
 
+    if (!user) {
+      return resp.status(400).json({
+        ok: false,
+        msg: "this user dont exists with this email ",
+      });
+    }
+
+    //Confirmar las contraseñas
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return resp.status(400).json({
+        ok: false,
+        msg: "Password incorrect",
+      });
+    }
+
+    //Generar nuestro JWT (Json web token)
+    const token = await generateJsonWebToken(user.id, user.name);
+
+    return resp.status(200).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return resp.status(500).json({
+      ok: false,
+      msg: "Please talk with the admin",
+    });
+  }
+};
+
+const revalidateToken = async (request, resp = response) => {
+  //   const uid = request.uid;
+  //   const name = request.name;
+
+  const { uid, name } = request;
+  const token = await generateJsonWebToken(uid, name);
+
+  return resp.status(200).json({
+    ok: true,
+    name,
+    uid,
+    token,
+  });
+};
 
 module.exports = {
-
-    createUser,
-    loginUser,
-    revalidateToken,
-
-}
+  createUser,
+  loginUser,
+  revalidateToken,
+};
